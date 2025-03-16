2c56bf4786322941557a6704512de3578cd00863
Arjun kumar vs <132894618+Arjunkumar1877@users.noreply.github.com>
2025-03-16 10:33:14 -0700

    fix: wating for conform-email

--- src/app/confirm-email/page.tsx
+++ src/app/confirm-email/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useState, useEffect } from 'react';
 import { useResendConfirmationEmail } from '@/api/mutations/useResendConfirmationEmail';
 import { Button } from '@/components/ui/button'
 import { useSearchParams } from 'next/navigation';
@@ -8,13 +9,45 @@ export default function ConfirmEmailPage() {
   const resendEmail = useResendConfirmationEmail();
   const searchParams = useSearchParams();
   const userId = searchParams.get('userId') || '';
+
+  const [isResendDisabled, setIsResendDisabled] = useState(false);
+  const [timer, setTimer] = useState(0);
+  const [lastResendTime, setLastResendTime] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (lastResendTime) {
+      const interval = setInterval(() => {
+        const currentTime = Date.now();
+        const remainingTime = Math.max(0, 300000 - (currentTime - lastResendTime));
+        setTimer(remainingTime);
+
+        if (remainingTime === 0) {
+          clearInterval(interval);
+          setIsResendDisabled(false);
+        }
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [lastResendTime]);
+
   const handleResendConfirmationEmail = async () => {
+    setIsResendDisabled(true);
+    setLastResendTime(Date.now());
+
     const res = await resendEmail.mutateAsync({ id: userId });
-    toast(res.message)
-  }
+    toast(res.message);
+  };
+
+  const formatTime = (milliseconds: number) => {
+    const minutes = Math.floor(milliseconds / 60000);
+    const seconds = Math.floor((milliseconds % 60000) / 1000);
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+
   return (
     <div className="min-w-full h-full flex flex-col justify-center items-center p-10">
-      <div className="min-w-[10rem] px-10  flex flex-col justify-center items-center gap-3 border-1 p-5 shadow-lg rounded-lg" >
+      <div className="min-w-[10rem] px-10 flex flex-col justify-center items-center gap-3 border-1 p-5 shadow-lg rounded-lg">
         <h1 className="text-3xl text-[#5F6A48]">Email Confirmation</h1>
 
         <p className="text-md text-[#5F6A48] text-center mt-4">
@@ -29,10 +62,11 @@ export default function ConfirmEmailPage() {
           type="button"
           className="max-w-[10rem] bg-[#5F6A48] text-white rounded-none mt-6 cursor-pointer"
           onClick={handleResendConfirmationEmail}
+          disabled={isResendDisabled}
         >
-          Resend Email
+          {isResendDisabled ? `Wait ${formatTime(timer)}` : 'Resend Email'}
         </Button>
       </div>
     </div>
-  )
+  );
 }
