49cc7f9a5426ebb857dec321a22b30585119967e
Arjun kumar vs <132894618+Arjunkumar1877@users.noreply.github.com>
2025-03-28 00:27:18 +0530

    feat: admin navbar

---
+++ src/app/admin/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/adminHeader/Navbar'
+import React from 'react'
+
+const DashBoard = () => {
+  return (
+    <div>
+       <Navbar />
+       
+    </div>
+  )
+}
+
+export default DashBoard
